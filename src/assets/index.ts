39604f9d3b36a244d6f82a9c5e53fcdb988caedd
Matt <nevermindthelabel@gmail.com>
2021-11-26 21:03:11 -0700

    import escaped keys

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,7 +1,9 @@
 import { letters } from './letters';
 import { numbers } from './numbers';
 import { symbols } from './symbols';
+import { escaped } from './escapedKeys';
 
 const allCharacters = [...letters, ...numbers, ...symbols];
 
 export default allCharacters;
+export { escaped };
