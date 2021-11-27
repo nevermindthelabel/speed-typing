7e4a4b5133f7adb93345a4cd4eaca15141f4c4d1
Matt <nevermindthelabel@gmail.com>
2021-11-26 20:52:17 -0700

    added additional logic

---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+import { letters } from './letters';
+import { numbers } from './numbers';
+import { symbols } from './symbols';
+
+const allCharacters = [...letters, ...numbers, ...symbols];
+
+export default allCharacters;
---
+++ src/assets/symbols.ts
@@ -0,0 +1,3 @@
+const symbols = [',', '.', '[', ']', '{', '}', '|', '&'];
+
+export { symbols };
