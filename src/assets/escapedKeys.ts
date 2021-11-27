3ab8e9572e5a773835cbdba7b11bb1b01a6cc286
Matt <nevermindthelabel@gmail.com>
2021-11-26 21:03:31 -0700

    initial list of keys to escape

---
+++ src/assets/escapedKeys.ts
@@ -0,0 +1,21 @@
+const escaped = [
+  'Shift',
+  'F1',
+  'F2',
+  'F3',
+  'F4',
+  'F5',
+  'F6',
+  'F7',
+  'F8',
+  'F9',
+  'F10',
+  'F11',
+  'F12',
+  'Escape',
+  'Delete',
+  'Backspace',
+  ' ',
+];
+
+export { escaped };
